9f63f7b561e2f713cfbffdb3b30c54fcacf39238
Len Huang <lenhuang720@gmail.com>
2020-02-14 23:37:22 -0500

    Add hover

---
+++ popup.js
@@ -0,0 +1,5 @@
+//import { productInfo } from './readsite.js';
+
+let pop = document.getElementById("price");
+//pop.innerHTML += "<p>" + productInfo.title + "</br>" + productInfo.price + "</p>";
+pop.innerHTML += "<p>" + "lol" + "</br>" + "xd" + " </p >";
