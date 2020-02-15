bfacf837cdba3d81e0ad7f8e7b24235ef92d0f75
Len Huang <lenhuang720@gmail.com>
2020-02-14 21:09:02 -0500

    Pre tartan hacks rehaul. (start from scratch)

--- content.js
+++ content.js
@@ -1,4 +1,24 @@
 // Content script is “a JavaScript file that runs
 // in the context of web pages.”
 
-alert("Hello from your mom");
+console.log("Working content scripts");
+
+let para = document.getElementById("priceblock_ourprice");
+para.style.cssText = "white: black; background-color: #A1ED8A";
+console.log(para.innerHTML)
+
+/*
+for (elt of para) {
+  //elt.innerHTML = "Pizza";
+  elt.style["background-color"] = "#A1ED8A";
+  myfunction(elt.innerHTML, "(\$d+.\d{2})");
+}
+*/
+/*
+function myfunction(htmlText, re) {
+  const regex = RegExp(re, 'g');
+  while ((match = regex.exec(htmlText)) != null) {
+    console.log("match found at " + match.index);
+  }
+}
+*/
